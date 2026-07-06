ae2162498fd5c58bf864fd4fc4b431a5b1a198c7
ButtKeC0de <Carlos_buttke@estudante.sesisenai.org.br>
2026-07-06 07:52:45 -0300

    add - atividade_um

---
+++ Lista_1/atv_1.php
@@ -0,0 +1,21 @@
+<?php
+
+
+function calcularFormula($x,$y){
+    
+    if ($x + $y == 0){
+            return"Não foi possivel realizar a divisão por zero";
+    }
+
+    $resultado = (pow($x,2) + pow($y,2)) / ($x + $y);
+
+    return $resultado;
+}
+
+    $x_usuario =9;
+    $y_usuario = 12;
+
+    echo "o valor de x é: $x_usuario <br>";
+    echo "o valor de y é: $y_usuario <br>";
+
+echo calcularFormula($x_usuario, $y_usuario);
