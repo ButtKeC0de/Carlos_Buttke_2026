2db8b9ad21202b26c4fea180ed5871688da17cb9
ButtKeC0de <Carlos_buttke@estudante.sesisenai.org.br>
2026-07-06 09:10:21 -0300

    add - atv_4

---
+++ Lista_1/atv_4.php
@@ -0,0 +1,30 @@
+<?php
+
+//Uma empresa deseja gerar senhas temporárias para seus colaboradores.
+//Crie uma função chamada gerarSenha() que receba a quantidade de caracteres
+//desejada e retorne uma senha aleatória contendo letras maiúsculas, minúsculas,
+//números e caracteres especiais.
+
+
+$tamanho = 8; 
+$alfabeto = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-=[]{}|;:,.<>?';
+
+
+function gerarSenha() {
+    global $tamanho, $alfabeto;
+    $senha = '';
+    $maxIndex = strlen($alfabeto) - 1;
+
+    for ($i = 0; $i < $tamanho; $i++) {
+        $index = rand(0, $maxIndex);
+        $senha .= $alfabeto[$index];
+    }
+
+    return $senha;
+    
+}
+
+
+echo "A senha gerada é: " . gerarSenha();
+
+?>
